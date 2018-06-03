d6e24ce0faca0a3d7925ef3c1d10b7edc753ce79
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2018-06-03 14:13:04 +0200

    Update build.gradle

--- app/src/androidTest/java/com/example/estefafdez/myfirstandroidapp/BaristaTest.java
+++ app/src/androidTest/java/com/example/estefafdez/myfirstandroidapp/BaristaTest.java
@@ -1,6 +1,18 @@
 package com.example.estefafdez.myfirstandroidapp;
 import android.util.Log;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions;
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions;
+import com.schibsted.spain.barista.interaction.BaristaEditTextInteractions;
+import com.schibsted.spain.barista.rule.BaristaRule;
+
+
 /**
  * Created by estefafdez
  * Test Class using Barista.
@@ -26,55 +38,56 @@ public class BaristaTest {
 
     @Test
     public void testTextoIsDisplayed(){
-        BaristaAssertions.assertDisplayed(R.id.texto);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.texto);
         Log.i("Info", "The element text is displayed.");
     }
 
     @Test
     public void testEditTextIsDisplayed(){
-        BaristaAssertions.assertDisplayed(R.id.editText);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.editText);
         Log.i("Info", "The element editText is displayed.");
     }
 
     @Test
     public void testButtonsAreDisplayed(){
-        BaristaAssertions.assertDisplayed(R.id.button1);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.button1);
         Log.i("Info", "The element button1 is displayed.");
-        BaristaAssertions.assertDisplayed(R.id.button2);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.button2);
         Log.i("Info", "The element button2 is displayed.");
     }
 
     @Test
     public void testClickOnButton1(){
         Log.i("Info", "Write a text on the edit text");
-        BaristaEditTextActions.writeToEditText(R.id.editText, "This is my first Barista Test");
+        BaristaEditTextInteractions.writeTo(R.id.editText, "This is my first Barista Test");
         Log.i("Info", "Click on the button1");
-        BaristaClickActions.click(R.id.button1);
+        BaristaClickInteractions.clickOn(R.id.button1);
         Log.i("Info", "Check that the text is displayed on the text view");
-        BaristaAssertions.assertDisplayed(R.string.textExample);
+        BaristaVisibilityAssertions.assertDisplayed(R.string.textExample);
     }
 
     @Test
     public void checkSecondActivityImages(){
         Log.i("Info", "Click on the Click Here button");
-        BaristaClickActions.click(R.id.buttonActivity);
+        BaristaClickInteractions.clickOn(R.id.buttonActivity);
         Log.i("Info", "Check the Android Img on the new activity");
-        BaristaAssertions.assertDisplayed(R.id.android_img);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.android_img);
         Log.i("Info", "Check the Barista Img on the new activity");
-        BaristaAssertions.assertDisplayed(R.id.barista_img);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.barista_img);
         Log.i("Info", "Return to the Main Activity");
-        BaristaClickActions.clickBack();
+        BaristaClickInteractions.clickBack();
     }
 
     @Test
     public void checkSecondActivityTexts(){
         Log.i("Info", "Click on the Click Here button");
-        BaristaClickActions.click(R.id.buttonActivity);
+        BaristaClickInteractions.clickOn(R.id.buttonActivity);
         Log.i("Info", "Check the Android text on the new activity");
-        BaristaAssertions.assertDisplayed(R.id.androidText);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.androidText);
         Log.i("Info", "Check the Barista text on the new activity");
-        BaristaAssertions.assertDisplayed(R.id.baristaText);
+        BaristaVisibilityAssertions.assertDisplayed(R.id.baristaText);
         Log.i("Info", "Return to the Main Activity");
-        BaristaClickActions.clickBack();
+        BaristaClickInteractions.clickBack();
     }
 }
+
